0e9c6b1597b80f7b9d7415de4cde5211b78c005a
tuddev <bubnika23@gmail.com>
2023-11-02 19:48:05 +0500

    re init

---
+++ api/index.js
@@ -0,0 +1,75 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 7071 });
+const clients = new Map();
+
+const state = {
+  room1: [],
+};
+
+wss.on("connection", (ws) => {
+  const id = uuidv4();
+  const initialState = "active";
+  const metadata = { id, state: initialState };
+
+  clients.set(ws, metadata);
+
+  ws.on("message", (messageAsString) => {
+    const message = JSON.parse(messageAsString);
+    const getUser = (id) => state.room1.find((user) => user.id === id);
+
+    const metadata = clients.get(ws);
+
+    message.sender = metadata.id;
+    let user = getUser(metadata.id);
+    if (!user) {
+      const newUser = {
+        id: metadata.id,
+        state: initialState,
+      };
+
+      state.room1.push(newUser);
+
+      user = getUser(metadata.id);
+    }
+
+    console.log(user);
+
+    switch (message.eventType) {
+      case "active":
+        user.state = "active";
+
+        break;
+      case "hover":
+        user.state = "hover";
+        break;
+      case "click":
+        user.state = "click";
+        break;
+      default:
+        break;
+    }
+
+    [...clients.entries()]
+      .filter(([key, user]) => !!getUser(user.id))
+      .map(([key, _]) => key)
+      .forEach((client) => {
+        client.send(JSON.stringify({ state: state.room1 }));
+      });
+  });
+});
+
+wss.on("close", (ws) => {
+  state.room1 = state.room1.filter((user) => user.id !== clients.get(ws).id);
+  clients.delete(ws);
+});
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+console.log("wss up");
---
+++ app/index.js
@@ -0,0 +1,60 @@
+(async function () {
+  const ws = await connectToServer();
+  const circle = document.querySelector("#circle");
+
+  ws.onmessage = (webSocketMessage) => {
+    const messageBody = JSON.parse(webSocketMessage.data);
+    console.log("messageBody", messageBody);
+    circle.className = "circle";
+    if (messageBody.state.every((user) => user.state === "active")) {
+      circle?.classList.add("active-both");
+      return;
+    }
+
+    if (messageBody.state.some((user) => user.state === "hover")) {
+      circle?.classList.add("hover-both");
+      return;
+    }
+  };
+
+  const messageBody = { roomId: "room1", eventType: "active" };
+  ws.send(JSON.stringify(messageBody));
+
+  circle?.addEventListener("mousedown", () => {
+    const messageBody = { roomId: "room1", eventType: "click" };
+    ws.send(JSON.stringify(messageBody));
+  });
+
+  document.body.addEventListener("mouseup", (event) => {
+    if (event?.target.id === "circle") {
+      const messageBody = { roomId: "room1", eventType: "hover" };
+      ws.send(JSON.stringify(messageBody));
+      return;
+    }
+
+    const messageBody = { roomId: "room1", eventType: "active" };
+    ws.send(JSON.stringify(messageBody));
+  });
+
+  circle?.addEventListener("mouseover", () => {
+    const messageBody = { roomId: "room1", eventType: "hover" };
+    ws.send(JSON.stringify(messageBody));
+  });
+
+  circle?.addEventListener("mouseout", () => {
+    const messageBody = { roomId: "room1", eventType: "active" };
+    ws.send(JSON.stringify(messageBody));
+  });
+
+  async function connectToServer() {
+    const ws = new WebSocket("ws://localhost:7071/ws");
+    return new Promise((resolve, reject) => {
+      const timer = setInterval(() => {
+        if (ws.readyState === 1) {
+          clearInterval(timer);
+          resolve(ws);
+        }
+      }, 10);
+    });
+  }
+})();
